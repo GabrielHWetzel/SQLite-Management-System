627042de48515633cdfd56362f4379dc4153648c
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-30 10:56:08 -0300

    Add Dialog box working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QGridLayout, QMainWindow, QDialog, QWidget,\
-    QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QDialog, QWidget, \
+    QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QComboBox
 from PyQt6.QtGui import QAction
 import sqlite3
 
@@ -8,25 +8,32 @@ import sqlite3
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        # Title
         self.setWindowTitle("Student Management System")
 
-        # Items
+        # Size
+        self.setMinimumSize(402, 500)
+
+        # Menu Items
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
 
-        # File menu
+        # File Menu
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.add_student)
         file_menu_item.addAction(add_student_action)
 
-        # Help menu
+        # Help Menu
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        # Main Table
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
-
         self.load_data()
 
     def load_data(self):
@@ -38,6 +45,60 @@ class MainWindow(QMainWindow):
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def add_student(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Title
+        self.setWindowTitle("Add Student")
+        self.setFixedSize(300, 150)
+
+        # Student Information
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+
+        self.mobile_number = QLineEdit()
+        self.mobile_number.setPlaceholderText("Phone Number")
+
+        submit = QPushButton("Submit")
+        submit.clicked.connect(self.add_student)
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.student_name)
+        layout.addWidget(self.course_name)
+        layout.addWidget(self.mobile_number)
+        layout.addWidget(submit)
+        self.setLayout(layout)
+
+    def add_student(self):
+        # Get Values
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile_number.text()
+
+        # Insert to Database
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        # Refresh
+        window.load_data()
 
 
 app = QApplication(sys.argv)
